ea2425e2b28ed3d19d4d5f4dbb0e338289d10fa2
Awais430 <hamzaaliics@gmail.com>
2024-06-26 07:20:55 -0700

    work on order details page

--- frontend/src/pages/Shop/ShopOrderDetails.jsx
+++ frontend/src/pages/Shop/ShopOrderDetails.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
+import OrderDetails from "../../components/Shop/OrderDetails.jsx"
+import Footer from "../../components/layout/Footer.jsx";
 const ShopCreateProduct = () => {
   return (
     <div>
       <DashboardHeader />
-      
+      <OrderDetails/>
+      <Footer/>
     </div>
   );
 };
