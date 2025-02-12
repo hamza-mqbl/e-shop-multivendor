abac6d25520e6ea579052b563535e2deafca91ab
hamza-mqbl <hamzaaliics@gmail.com>
2025-02-12 12:58:58 +0500

    frontend changed

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -2,9 +2,9 @@
 // export const server="http://13.60.95.182:8000/api/v2"
 
 
-export const server="https://e-shop-multivendor.onrender.com/api/v2"
+export const server="https://e-shop-server-zeta.vercel.app/api/v2"
 
 // export const backend_url="http://localhost:8000/"
 // export const backend_url="http://13.60.95.182:8000/"
 
-export const backend_url="https://e-shop-multivendor.onrender.com"
+export const backend_url="https://e-shop-server-zeta.vercel.app"
