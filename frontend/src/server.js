d7347d5a90342c4ca6eb887810ddf824d4c3cf10
hamza-mqbl <hamzaaliics@gmail.com>
2025-02-12 13:30:46 +0500

    after setting changed

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,6 @@
 // export const server="http://localhost:8000/api/v2"
 // export const server="http://13.60.95.182:8000/api/v2"
-
+// a
 
 export const server="https://e-shop-server-zeta.vercel.app/api/v2"
 
